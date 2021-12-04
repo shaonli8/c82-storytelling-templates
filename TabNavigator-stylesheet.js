ab2c35828e7e48180b44ed30ad8535fdab0da0b6
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-12-04 11:11:40 +0530

    Create TabNavigator-stylesheet.js

---
+++ TabNavigator-stylesheet.js
@@ -0,0 +1,14 @@
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
